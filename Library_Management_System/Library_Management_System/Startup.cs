cbec65c4336dc627312615acf9ba679fee130493
Shubham Gupta <311297sg@gmail.com>
2022-06-23 11:37:11 +0530

    third

--- Library_Management_System/Library_Management_System/Startup.cs
+++ Library_Management_System/Library_Management_System/Startup.cs
@@ -31,7 +31,7 @@ namespace Library_Management_System
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("constr")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
